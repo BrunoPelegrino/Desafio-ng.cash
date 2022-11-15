2f3373240f0c66e9ea558d8d3763819b6f71e3ec
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2022-11-15 00:03:15 -0300

    feat: model user created/ saving daily changes

---
+++ backend/src/database/models/User.ts
@@ -0,0 +1,38 @@
+import { INTEGER, STRING, Model } from 'sequelize';
+import db from '.'
+
+class User extends Model {
+  private id!: number;
+  private username!: string;
+  private password!: string;
+  private accountId!: number;
+
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: { 
+    type: STRING,
+    allowNull: false,
+  },
+  accountId : { 
+    type: INTEGER,
+    allowNull: false,
+  }
+},{
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+User.belongsTo(Account, {foreignKey: 'accountId', as: 'user'})
